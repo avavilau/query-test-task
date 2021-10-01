523ad085bab3a74168181c597514083ed177072f
Aliaksei Vavilau <Aliaksei_Vavilau@ls2.io>
2021-10-01 21:12:21 +0200

    Output format, adjustment for group by

--- src/main/java/org/query/calc/QueryCalcImpl.java
+++ src/main/java/org/query/calc/QueryCalcImpl.java
@@ -6,11 +6,13 @@ import java.nio.file.Path;
 public class QueryCalcImpl implements QueryCalc {
     @Override
     public void select(Path t1, Path t2, Path t3, Path output) throws IOException {
-        // t1 is a file contains table "t1" with two columns "a" and "x". First line is a number of rows, then each line
-        // contains exactly one row, that contains two numbers parsable by Double.parse(): value for column a and x
-        // respectively.See test resources for examples.
-        // t2 is a file contains table "t2" with columns "b" and "y". Same format.
-        // t3 is a file contains table "t3" with columns "c" and "z". Same format.
+        // - t1 is a file contains table "t1" with two columns "a" and "x". First line is a number of rows, then each
+        //  line contains exactly one row, that contains two numbers parsable by Double.parse(): value for column a and
+        //  x respectively.See test resources for examples.
+        // - t2 is a file contains table "t2" with columns "b" and "y". Same format.
+        // - t3 is a file contains table "t3" with columns "c" and "z". Same format.
+        // - output is table stored in the same format: first line is a number of rows, then each line is one row that
+        //  contains two numbers: value for column a and s.
         //
         // Number of rows of all three tables lays in range [0, 1_000_000].
         // It's guaranteed that full content of all three tables fits into RAM.
