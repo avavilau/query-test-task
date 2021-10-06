baa6699a2f12c66671fe0f5bc5babbd5d9c3f849
Ivan Taleika <ivan.taleika_ext@sonepar.com>
2021-10-07 00:05:19 +0200

    Added tree map implementation with O(n) = 2 * n^2 * log(n)

--- src/main/java/org/query/calc/QueryCalcImpl.java
+++ src/main/java/org/query/calc/QueryCalcImpl.java
@@ -1,9 +1,19 @@
 package org.query.calc;
 
+import it.unimi.dsi.fastutil.doubles.Double2DoubleRBTreeMap;
+import it.unimi.dsi.fastutil.doubles.Double2DoubleSortedMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class QueryCalcImpl implements QueryCalc {
+
+    private static final int LIMIT = 10;
+
     @Override
     public void select(Path t1, Path t2, Path t3, Path output) throws IOException {
         // - t1 is a file contains table "t1" with two columns "a" and "x". First line is a number of rows, then each
@@ -34,5 +44,84 @@ public class QueryCalcImpl implements QueryCalc {
         // Note: STABLE is not a standard SQL command. It means that you should preserve the original order. 
         // In this context it means, that in case of tie on s-value you should prefer value of a, with a lower row number.
         // In case multiple occurrences, you may assume that group has a row number of the first occurrence.
+
+        Row[] values1 = readTable(t1);
+        Row[] values2 = readTable(t2);
+        Row[] values3 = readTable(t3);
+
+        // n1 * log(n1)
+        Arrays.sort(values1, Comparator.comparingDouble(Row::getKey).reversed());
+
+        Double2DoubleRBTreeMap sumMap = new Double2DoubleRBTreeMap(Comparator.reverseOrder());
+        // n2 * n3 * log(n2 * n3)
+        for (Row r1 : values2) {
+            for (Row r2 : values3) {
+                double sum = r1.getKey() + r2.getKey();
+                double mult = r1.getValue() * r2.getValue();
+                sumMap.mergeDouble(sum, mult, Double::sum);
+            }
+        }
+
+        Row[] answer = new Row[values1.length];
+
+        int i = 0;
+        int outputSize = 0;
+        double minBc = Double.MAX_VALUE;
+        double yzSum = 0d;
+        while (i < values1.length) {
+            Row row = values1[i++];
+            double key = row.getKey();
+            Double2DoubleSortedMap deltaJoinRecords = sumMap.subMap(minBc, key);
+//            // subMap may include key, need to remove it, if present
+
+            if (!deltaJoinRecords.isEmpty()) {
+                yzSum += deltaJoinRecords.values().doubleStream().sum() - deltaJoinRecords.getOrDefault(minBc, 0d);
+                minBc = deltaJoinRecords.lastDoubleKey();
+            }
+
+            double aggregatedXs = row.getValue();
+            while (i < values1.length && values1[i].getKey() == key) {
+                aggregatedXs += values1[i++].getValue();
+            }
+
+            answer[outputSize++] = new Row(key, aggregatedXs * yzSum, row.getRowNumber());
+        }
+
+        Arrays.sort(answer, 0, outputSize,
+                Comparator.comparingDouble(Row::getValue).reversed().thenComparingInt(Row::getRowNumber));
+        if (outputSize > LIMIT) {
+            answer = Arrays.copyOfRange(answer, 0, LIMIT);
+            outputSize = LIMIT;
+        }
+        writeResult(output, answer, outputSize);
+    }
+
+    private Row[] readTable(Path p) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(p)) {
+            int nEntries = Integer.parseInt(reader.readLine());
+            Row[] table = new Row[nEntries];
+            String line;
+            int recordId = 0;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                table[recordId] = new Row(Double.parseDouble(split[0]), Double.parseDouble(split[1]), recordId + 1);
+                recordId++;
+            }
+            return table;
+        }
+    }
+
+    private void writeResult(Path output, Row[] result, int size) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(output)) {
+            writer.write(Integer.toString(size));
+            writer.newLine();
+
+            for (Row r : result) {
+                writer.write(String.format("%.6f %.6f", r.getKey(), r.getValue()));
+                writer.newLine();
+            }
+
+            writer.flush();
+        }
     }
 }
---
+++ src/main/java/org/query/calc/Row.java
@@ -0,0 +1,11 @@
+package org.query.calc;
+
+import lombok.Data;
+
+@Data
+public class Row {
+
+    private final double key;
+    private final double value;
+    private final int rowNumber;
+}
