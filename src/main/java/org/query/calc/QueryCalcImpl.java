447ad5746ff1c7b2cc04392a3d33cd9095b91f42
Aliaksandr Stsiapanay <aliaksandrstsiapanay@cloud.upwork.com>
2021-10-06 09:45:13 +0300

    Optimize performance

--- src/main/java/org/query/calc/QueryCalcImpl.java
+++ src/main/java/org/query/calc/QueryCalcImpl.java
@@ -9,12 +9,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class QueryCalcImpl implements QueryCalc {
+    /**
+     * Implementation notes. Lets assume three tables have n1, n2 and n3 number of records accordingly.
+     * Time complexity O(n2 * n3 + n1 * Log(n2 * n3))
+     * Space complexity O(n1 + n2 * n3)
+     */
     @Override
     public void select(Path t1, Path t2, Path t3, Path output) throws IOException {
         // - t1 is a file contains table "t1" with two columns "a" and "x". First line is a number of rows, then each
@@ -46,7 +52,10 @@ public class QueryCalcImpl implements QueryCalc {
         // In this context it means, that in case of tie on s-value you should prefer value of a, with a lower row number.
         // In case multiple occurrences, you may assume that group has a row number of the first occurrence.
 
-        List<double[]> l1 = readTable(t1, true), l2 = readTable(t2, false), l3 = readTable(t3, false);
+        // could be read in parallel
+        //List<List<double[]>> l23 = Stream.of(t2, t3).parallel().map(this::readTable).collect(Collectors.toList());
+        List<double[]> l2 = readTable(t2), l3 = readTable(t3);
+        Map<Double, double[]> l1 = readAndGroupTable(t1);
 
         List<double[]> merged = l2.parallelStream()
                 .flatMap(r1 -> l3.parallelStream().map(r2 -> {
@@ -54,20 +63,22 @@ public class QueryCalcImpl implements QueryCalc {
                     res[0] = r1[0] + r2[0]; // b + c
                     res[1] = r1[1] * r2[1]; // y * z
                     return res;
-                })).collect(Collectors.toList());
+                })).sorted(Comparator.comparingDouble(a -> a[0])).collect(Collectors.toList());
 
-        List<Map.Entry<Key, Double>> resultSet = l1.parallelStream().peek(r1 -> r1[2] = r1[2] * merged.stream()
-                // a < b + c
-                .filter(r -> r1[1] < r[0]).map(r -> r[1]).reduce(0.0, Double::sum))
-                .collect(Collectors.groupingByConcurrent(r -> new Key(r[1], r[0]), // GROUP BY a
-                        Collectors.summingDouble(r -> r[2]))) // SUM(x * y * z)
-                .entrySet().parallelStream()
+        double [] prefixSum = new double[merged.size()];
+        prefixSum[0] = merged.get(0)[1];
+        for(int i = 1; i < merged.size(); i++) {
+            prefixSum[i] = prefixSum[i - 1] + merged.get(i)[1];
+        }
+
+        List<Map.Entry<Double, double[]>> resultSet = l1.entrySet().parallelStream()
+                .peek(entry -> entry.getValue()[1] *= calcSum(entry.getKey(), merged, prefixSum))
                 .sorted((a, b) -> {
                     // ORDER BY s DESC
-                    int cmp = b.getValue().compareTo(a.getValue());
+                    int cmp = Double.compare(b.getValue()[1], a.getValue()[1]);
                     // make sorting stable
                     if (cmp == 0) {
-                        return Double.compare(a.getKey().rowNumber, b.getKey().rowNumber);
+                        return Double.compare(a.getValue()[0], b.getValue()[0]);
                     }
                     return cmp;
                 }).limit(10).collect(Collectors.toList());
@@ -75,46 +86,68 @@ public class QueryCalcImpl implements QueryCalc {
         writeResultToTable(output, resultSet);
     }
 
-    private static class Key {
-        private final double value;
-        private final double rowNumber;
-
-        public Key(double value, double rowNumber) {
-            this.value = value;
-            this.rowNumber = rowNumber;
+    private double calcSum(double key, List<double[]> merged, double [] prefixSum) {
+        int n = prefixSum.length;
+        int high = n - 1, low = 0, index = -1;
+        // do binary search to find strictly greater element
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if (merged.get(mid)[0] <= key) {
+                low = mid + 1;
+            } else {
+                index = mid;
+                high = mid - 1;
+            }
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Key key = (Key) o;
-            return Double.compare(key.value, value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(value);
+        if (index == -1) {
+            return 0.0;
+        } else {
+            // calculate sum using prefixSum array
+            return index == 0 ? prefixSum[n - 1] : prefixSum[n - 1] - prefixSum[index - 1];
         }
     }
 
-    private void writeResultToTable(Path pathToTable, List<Map.Entry<Key, Double>> resultSet) throws IOException {
+    private void writeResultToTable(Path pathToTable, List<Map.Entry<Double, double[]>> resultSet) throws IOException {
         try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(pathToTable.toFile())))) {
             bw.write(Long.toString(resultSet.size()));
             if (resultSet.isEmpty()) return;
             bw.newLine();
             for (int i = 0; i < resultSet.size(); i++) {
-                Map.Entry<Key, Double> entry = resultSet.get(i);
-                bw.write(Double.toString(entry.getKey().value));
+                Map.Entry<Double, double[]> entry = resultSet.get(i);
+                bw.write(entry.getKey().toString());
                 bw.write(" ");
-                bw.write(entry.getValue().toString());
+                bw.write(Double.toString(entry.getValue()[1]));
                 if (i != resultSet.size() - 1)
                     bw.newLine();
             }
         }
     }
 
-    private List<double[]> readTable(Path pathToTable, boolean returnRowNumber) throws IOException {
+    private Map<Double, double[]> readAndGroupTable(Path pathToTable) throws IOException {
+        Map<Double, double[]> content;
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(new FileInputStream(pathToTable.toFile())))) {
+            int count = Integer.parseInt(br.readLine());
+            content = new HashMap<>();
+            int rowNumber = 1;
+            while (rowNumber <= count) {
+                String[] parts = br.readLine().split(" ");
+                double a = Double.parseDouble(parts[0]);
+                double x = Double.parseDouble(parts[1]);
+                double [] e = content.get(a);
+                if (e == null) {
+                    e = new double[]{rowNumber, x};
+                    content.put(a, e);
+                } else
+                    e[1] += x;
+                content.put(a, e);
+                rowNumber++;
+            }
+        }
+        return content;
+    }
+
+    private List<double[]> readTable(Path pathToTable) throws IOException {
         List<double[]> content;
         try (BufferedReader br
                      = new BufferedReader(new InputStreamReader(new FileInputStream(pathToTable.toFile())))) {
@@ -123,10 +156,7 @@ public class QueryCalcImpl implements QueryCalc {
             int rowNumber = 1;
             while (rowNumber <= count) {
                 String[] parts = br.readLine().split(" ");
-                if (returnRowNumber)
-                    content.add(new double[]{rowNumber, Double.parseDouble(parts[0]), Double.parseDouble(parts[1])});
-                else
-                    content.add(new double[]{Double.parseDouble(parts[0]), Double.parseDouble(parts[1])});
+                content.add(new double[]{Double.parseDouble(parts[0]), Double.parseDouble(parts[1])});
                 rowNumber++;
             }
         }
